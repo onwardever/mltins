c0fb3a7cdf69a9c6a03ce7bae756846b7863cf05
yangdian <2406616734@qq.com>
2023-05-15 10:54:09 +0800

    init

--- main.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-x1 = np.array([1, 1, 1])
-x2 = np.array([2, 2, 2])
-
-print(np.linalg.norm(x1 - x2, ord=2))
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='mltins',
     version='0.0.1',
-    description='A project for matching learing.',
+    description='A project for matching learning.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/onwardever/mltins.git',
